53f4327bdc1b9ed5969411ab35da0bd1beb703e3
Enio Silveira Leite <eniosleite@yahoo.com.br>
2025-02-25 10:59:23 -0300

    estilizando a sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,7 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => (
+  <FotoPerfil src="https://avatars.githubusercontent.com/u/80004957?s=400&u=ee0e0070c2418d249e6defc42bba4b1a8a81eb48&v=4" />
+)
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://avatars.githubusercontent.com/u/80004957?v=4" />
-    <Titulo fontSize={20}>Enio Silveira Leite</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Enio Silveira Leite</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        EnioSL
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro Front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
